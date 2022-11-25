0039c790ccbb6e2a24c1b217d1729863ff91c44b
efrxngg <efrenpgc2602@gmail.com>
2022-11-24 20:17:27 -0500

    detailts

--- src/main/java/com/empresax/core/application/openapi/OpenApiDoc.java
+++ src/main/java/com/empresax/core/application/openapi/OpenApiDoc.java
@@ -15,18 +15,18 @@ public class OpenApiDoc {
     @Bean
     public OpenAPI infoOpenApi() {
         return new OpenAPI()
-                .info(new Info().title("Ecommerce API RESTful")
-                        .description("This project was developed for learning porpouses, if you found any problem and you want to notify me or want to contact me yo can do it through my Linkedln")
+                .info(new Info().title("E-commerce API REST")
+                        .description("Este proyecto fue desarrollado con fines de aprendizaje si encuentras algún error o deseas comunicarte conmigo puedes hacérmelo saber mediante mi LinkedIn, el repository del proyecto tambien se encuentra en mi perfil. <br><br><b>Instrucciones de uso:</b> <br>1.- Registrate en el apartado de usuario. <br>2.- Authenticate y coloca el token devuelto en el candadito de la parte superior. <br>3.- En el apartado de Productos escoja los ids que desees agregar a tu carrito. <br>4.- Cree un Carrito en el apartado de Cart con los ids y la cantidad de de ese producto que desee. <br>5.- Por ultimo puede darle a generar factura para obtener el valor total a cancelar.")
                         .version("v1")
                         .license(
                                 new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0.html"))
                         .contact(new Contact()
-                                .name("Efren Galarza")
-                                .email("efrenpgc2602@gmail.com")
-                                .url("https://github.com/efrxngg")))
+                                .name("LinkedIn")
+                                // .email("efrenpgc2602@gmail.com")
+                                .url("https://www.linkedin.com/in/efren-galarza-chavez-a7b245243/")))
                 .externalDocs(new ExternalDocumentation()
                         .description("by @efrxngg")
-                        .url("https://github.com/efrxngg?tab=repositories"));
+                        .url("https://www.linkedin.com/in/efren-galarza-chavez-a7b245243/"));
     }
 
     @Bean
